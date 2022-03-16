c6aecaf15693ec091c0a743113f7dd55b754f318
Andrea <onemanshow2099@gmail.com>
2022-03-16 16:41:25 +0100

    snack2 completato

--- Snack2/script.js
+++ Snack2/script.js
@@ -1,7 +1,7 @@
-let numeroNome = Math.floor(Math.random() * 4) +1;
+let numeroNome = Math.floor(Math.random() * 8) +1;
 
 
-let numeroCognome = Math.floor(Math.random() * 4) +1;
+let numeroCognome = Math.floor(Math.random() * 8) +1;
 
 console.log(numeroNome)
 console.log(numeroCognome)
@@ -14,6 +14,14 @@ let nome3 = "Viola";
 
 let nome4 = "Grisù";
 
+let nome5 = "Bud";
+
+let nome6 = "Terence";
+
+let nome7 = "Ubaldo";
+
+let nome8 = "Bimbo";
+
 let cognome1 = "Boseghin";
 
 let cognome2 = "Lamarmora";
@@ -21,6 +29,14 @@ let cognome2 = "Lamarmora";
 let cognome3 = "DioSardo";
 
 let cognome4 = "Peppone";
+
+let cognome5 = "Brillante";
+
+let cognome6 = "Muchacha";
+
+let cognome7 = "Billo-Ballo";
+
+let cognome8 = "Gleyzer";
 
 let nomescritto ;
 let cognomescritto;
@@ -31,7 +47,24 @@ if (numeroNome == 1) {
     nomescritto = nome2;
 } else if (numeroNome == 3) {
     nomescritto = nome3;
-} else {
+} 
+else if (numeroNome == 5) {
+    nomescritto = nome5;
+}
+
+else if (numeroNome == 6) {
+    nomescritto = nome6;
+}
+
+else if (numeroNome == 7) {
+    nomescritto = nome7;
+}
+
+else if (numeroNome == 8) {
+    nomescritto = nome8;
+}
+
+else {
     nomescritto = nome4;
 }
 
@@ -41,7 +74,24 @@ if (numeroCognome == 1) {
     cognomescritto = cognome2;
 } else if (numeroCognome == 3) {
     cognomescritto = cognome3;
-} else {
+} 
+else if (numeroCognome == 5) {
+    cognomescritto = cognome5;
+}
+
+else if (numeroCognome == 6) {
+    cognomescritto = cognome7;
+}
+
+else if (numeroCognome == 7) {
+    cognomescritto = cognome7;
+}
+
+else if (numeroCognome == 8) {
+    cognomescritto = cognome8;
+}
+
+else {
     cognomescritto = cognome4;
 }
 
