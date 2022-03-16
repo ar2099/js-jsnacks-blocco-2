a0efbf7832b411d9a5a70d6fabf42a52a42a4377
Andrea <onemanshow2099@gmail.com>
2022-03-16 16:09:12 +0100

    esercizio 2 quasi completo

--- Snack2/script.js
+++ Snack2/script.js
@@ -4,6 +4,7 @@ let numeroNome = Math.floor(Math.random() * 4) +1;
 let numeroCognome = Math.floor(Math.random() * 4) +1;
 
 console.log(numeroNome)
+console.log(numeroCognome)
 
 let nome1 = "Andrea";
 
@@ -21,9 +22,31 @@ let cognome3 = "DioSardo";
 
 let cognome4 = "Peppone";
 
+let nomescritto ;
+let cognomescritto;
+
+if (numeroNome == 1) {
+    nomescritto = nome1;
+} else if (numeroNome == 2) {
+    nomescritto = nome2;
+} else if (numeroNome == 3) {
+    nomescritto = nome3;
+} else {
+    nomescritto = nome4;
+}
+
+if (numeroCognome == 1) {
+    cognomescritto = cognome1;
+} else if (numeroCognome == 2) {
+    cognomescritto = cognome2;
+} else if (numeroCognome == 3) {
+    cognomescritto = cognome3;
+} else {
+    cognomescritto = cognome4;
+}
 
 
 
-document.getElementById("vFrase").innerHTML = numeroNome ;
+document.getElementById("vFrase").innerHTML = nomescritto ;
 
-document.getElementById("vFrase2").innerHTML = numeroCognome ;
+document.getElementById("vFrase2").innerHTML = cognomescritto ;
