2e420745d18da37962c80743d7834c4373d30ede
Andrea <onemanshow2099@gmail.com>
2022-03-16 15:35:51 +0100

    esercizio2 quasi completo

---
+++ Snack2/script.js
@@ -0,0 +1,29 @@
+let numeroNome = Math.floor(Math.random() * 4) +1;
+
+
+let numeroCognome = Math.floor(Math.random() * 4) +1;
+
+console.log(numeroNome)
+
+let nome1 = "Andrea";
+
+let nome2 = "Beppe";
+
+let nome3 = "Viola";
+
+let nome4 = "Grisù";
+
+let cognome1 = "Boseghin";
+
+let cognome2 = "Lamarmora";
+
+let cognome3 = "DioSardo";
+
+let cognome4 = "Peppone";
+
+
+
+
+document.getElementById("vFrase").innerHTML = numeroNome ;
+
+document.getElementById("vFrase2").innerHTML = numeroCognome ;
