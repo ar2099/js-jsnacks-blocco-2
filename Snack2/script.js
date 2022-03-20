34881a92dcf03ceca7fef1513d4eab3f2874e848
Andrea <onemanshow2099@gmail.com>
2022-03-20 15:30:34 +0100

    01 rifacimento esercizio 2

---
+++ Snack2/script.js
@@ -0,0 +1,13 @@
+let nomi = ["Bob", "Andrea", "Maria", "Chiara", "Beppe", "Gestaldo"];
+
+let cognomi = ["Boseghin", "Actis", "Borghezio", "Camino", "Trentalance", "Drello"];
+
+let c = Math.floor(Math.random()*6);
+
+c = Number(c);
+
+console.log(c)
+
+console.log(nomi[c])
+console.log(cognomi[c])
+
