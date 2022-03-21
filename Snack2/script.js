6673b4f090c1aa3f250858788faa0be7b9bd24c4
Andrea <onemanshow2099@gmail.com>
2022-03-21 09:30:58 +0100

    rifacimento esercizio 2 completato

--- Snack2/script.js
+++ Snack2/script.js
@@ -2,15 +2,22 @@ let nomi = ["Bob", "Andrea", "Maria", "Chiara", "Beppe", "Gestaldo"];
 
 let cognomi = ["Boseghin", "Actis", "Borghezio", "Camino", "Trentalance", "Drello"];
 
-let c = Math.floor(Math.random()*6);
-let d = Math.floor(Math.random() * 6);
-
-c = Number(c);
-
-console.log(c)
-
-console.log(nomi[c])
-console.log(cognomi[d])
 
 
+
+let bottone = document.getElementById("bottone");
+let numero = 0;
+numero = Number(numero)
+bottone.addEventListener("click",
+
+function(){
+    let c = Math.floor(Math.random() * 6);
+    let d = Math.floor(Math.random() * 6);
+    c = Number(c);
+    d = Number(d);
+    numero = numero +1;
+    document.getElementById("lista-invitati").innerHTML += `<div class="riga"><div class="numero-part"><p>${numero}</p></div><div class="nome-part"><p>${cognomi[c]} ${nomi[d]}</p></div></div>`
+}
+
+)
 
